7b8194b66c2040aeb208f1a4d1f8b8bf139dd8ae
RafaPiovezan <rafaella.filipe@sptech.school>
2022-04-11 20:47:17 -0300

    aula 04

--- music-box-react/src/pages/Musicas.jsx
+++ music-box-react/src/pages/Musicas.jsx
@@ -1,12 +1,23 @@
-import React from "react";
 import ItemMusica from '../components/ItemMusica';
 import Menu from "../components/Menu";
+import React, { useState, useEffect } from "react"
+import api from '../api'
 
 function Musicas() {
+  const [musicas, setMusicas] = useState([]);
+  useEffect(() => {
+    api.get().then((res) => {
+      setMusicas(res.data)
+    })
+      .catch((err) => {
+        console.log(err)
+      })
+  },[])
+
   return (
     <>
       <Menu />
-  
+
       <div className="container">
         <div className="filter">
           <button className="btn">Adicionar</button>
@@ -15,21 +26,19 @@ function Musicas() {
 
       <div className="container">
         <div className="music-boxes">
+          {
+            musicas.map(musica => (
+              <ItemMusica
+                musica={musica.musica}
+                artista={musica.artista}
+                genero={musica.categoria}
+                ano={musica.ano}
+                id={musica.id}
+                key = {musica.id}
+              />
+            ))
+          }
 
-          <ItemMusica 
-            musica="In the end"
-            artista="Linkin Park"
-            genero="Rock do bão"
-            ano={2001}
-            id="1"
-          />
-          <ItemMusica 
-            musica="Ela Partiu"
-            artista="Tim Maia"
-            genero="Sofrência da boa"
-            ano={1997}
-            id="1"
-          />
 
         </div>
       </div>
